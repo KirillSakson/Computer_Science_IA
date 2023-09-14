243ad6dbb2ae1114d3dee6757ca65b47d7e58a67
KirillSakson <144152917+KirillSakson@users.noreply.github.com>
2023-09-14 22:50:10 +0200

    first commit __init__

---
+++ __init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from config import Config
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+from app.controllers import home_controller
+
+
+app.run(debug=True)
